bcc74be7901655171422fdefceddaf19758e6dd2
Paolo Angioletti <85297193+pangiole-hmrc@users.noreply.github.com>
2021-07-09 14:06:19 +0100

    Introduce minimal entity-resolver connector and model

---
+++ app/uk/gov/hmrc/channelpreferences/connectors/EntityResolverConnector.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.channelpreferences.connectors
+
+import javax.inject.{ Inject, Singleton }
+import play.api.Configuration
+import uk.gov.hmrc.channelpreferences.model.Entity
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import uk.gov.hmrc.http.HttpClient
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+@Singleton
+class EntityResolverConnector @Inject()(config: Configuration, httpClient: HttpClient)(implicit ec: ExecutionContext)
+    extends ServicesConfig(config) {
+
+  private val serviceUrl = baseUrl("entity-resolver")
+
+  def resolveBy(id: String)(implicit hc: HeaderCarrier): Future[Entity] = {
+    import uk.gov.hmrc.http.HttpReads.Implicits._
+    httpClient.GET[Entity](s"$serviceUrl/entity-resolver/$id")
+  }
+
+  // TODO def update(entity: Entity): Future[Entity]
+}
---
+++ app/uk/gov/hmrc/channelpreferences/model/Entity.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.channelpreferences.model
+
+import play.api.libs.json.{ Json, OFormat }
+
+/**
+  * It represents a taxable entity whose tax identifiers are retrievable from the entity-resolver service
+  *
+  * @param id is the unique identifier, normally used as lookup key
+  * @param nino is the (maybe) National Insurance Number
+  * @param saUtr is the (maybe) Self Assessment Unique Tax Reference
+  * @param itsa is the (maybe) Income Tax Self Assessment identifier
+  */
+final case class Entity(id: String, saUtr: Option[String], nino: Option[String], itsa: Option[String])
+
+object Entity {
+  implicit val format: OFormat[Entity] = Json.format[Entity]
+}
