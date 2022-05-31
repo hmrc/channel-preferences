484f8e3290334b58481d46d32cb931fc2bfe27e4
Satya Josyula <74477958+satyajosyula2020@users.noreply.github.com>
2022-05-31 18:01:05 +0100

    DC-4294 : Add decryption to ITSA /channel-preferences/confirm (#83)
    * DC-4294 : Add decryption to ITSA /channel-preferences/confirm
    
    * Change the encryption key

---
+++ test/uk/gov/hmrc/channelpreferences/utils/EntityIdCryptoSpec.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.channelpreferences.utils
+
+import org.scalatestplus.play.PlaySpec
+
+class EntityIdCryptoSpec extends PlaySpec with EntityIdCrypto {
+
+  "encryptString" must {
+    "Successfully parse a string " in {
+      encryptString("f0ea9b13-82bf-4d1a-9619-bb1e2481f8d8") mustBe Left(
+        "YL4AfL0xsXviYiQtibOWOZVqZlKLwsna7Xq4XsFOLjkdcBJd5KldTXzrL81IsrEZ")
+    }
+
+  }
+
+  "decryptString" must {
+    "Successfully parse a string " in {
+      decryptString("YL4AfL0xsXviYiQtibOWOZVqZlKLwsna7Xq4XsFOLjkdcBJd5KldTXzrL81IsrEZ") mustBe Left(
+        "f0ea9b13-82bf-4d1a-9619-bb1e2481f8d8")
+    }
+
+  }
+
+}
