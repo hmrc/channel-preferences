644c64f97e67e1b1143b1a7c7191b8adafc104c6
Andrew Walker <andrew.walker@digital.hmrc.gov.uk>
2022-10-21 10:56:41 +0100

    DC-4517: Removed mockserver-netty, does not appear to be required

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.7.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.8.0",
     "com.typesafe.play" %% "play-json-joda"            % "2.9.3",
     "org.typelevel"     %% "cats-core"                 % "2.8.0",
   )
@@ -16,8 +16,8 @@ object AppDependencies {
   val dependencyOverrides: Seq[ModuleID] = Seq()
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "7.7.0"         % Test,
-    "com.typesafe.play"      %% "play-test"                % "2.8.17"        % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "7.8.0"         % Test,
+    "com.typesafe.play"      %% "play-test"                % "2.8.18"        % Test,
     "org.scalactic"          %% "scalactic"                % "3.2.14"        % Test,
     "org.scalatest"          %% "scalatest"                % "3.2.14"        % "test, it",
     "org.scalacheck"         %% "scalacheck"               % "1.17.0"        % Test,
@@ -27,7 +27,6 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "service-integration-test" % "1.3.0-play-28" % "test, it",
     "org.mockito"            %% "mockito-scala"            % "1.17.12"       % "test, it",
     "com.github.tomakehurst" % "wiremock"                  % "2.33.2"        % "test, it",
-    "org.mock-server"        % "mockserver-netty"          % "5.14.0"        % "test, it",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.64.0"        % "test, it",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.4"      % "test, it",
   )
