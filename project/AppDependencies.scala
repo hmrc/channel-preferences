6a26f4ce957b6147f4b71bff0c125d27aead82ef
Andrew Walker <andrew.walker@digital.hmrc.gov.uk>
2023-05-11 17:11:57 +0100

    Dc 5023 - channel preferences maintenance (#101)
    * Copyright year bump
    
    * DC-5023: Maintenance updates
    
    * DC-5023: Address PR bot comments
    
    * DC-5023: Address PR comments
    
    * DC-5023: Adding back SbtDistributablesPlugin
    
    * DC-5023: Disable JUnitXmlReportPlugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,12 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.15.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.8.0",
-    "com.typesafe.play" %% "play-json-joda"            % "2.9.3",
-    "org.typelevel"     %% "cats-core"                 % "2.8.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
+    "com.typesafe.play" %% "play-json-joda"            % "2.9.4",
+    "org.typelevel"     %% "cats-core"                 % "2.9.0",
   )
 
   // Several dependencies require a lower major version of Scala 2.13 xml library
@@ -17,18 +19,18 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "7.8.0"         % Test,
-    "com.typesafe.play"      %% "play-test"                % "2.8.18"        % Test,
-    "org.scalactic"          %% "scalactic"                % "3.2.14"        % Test,
-    "org.scalatest"          %% "scalatest"                % "3.2.14"        % "test, it",
-    "org.scalacheck"         %% "scalacheck"               % "1.17.0"        % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
-    "org.scalatestplus"      %% "scalacheck-1-15"          % "3.2.11.0"      % "test, it",
-    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"      % "test, it",
-    "uk.gov.hmrc"            %% "service-integration-test" % "1.3.0-play-28" % "test, it",
-    "org.mockito"            %% "mockito-scala"            % "1.17.12"       % "test, it",
-    "com.github.tomakehurst" % "wiremock"                  % "2.33.2"        % "test, it",
-    "com.vladsch.flexmark"   % "flexmark-all"              % "0.64.0"        % "test, it",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.14.1"      % "test, it",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"    % bootstrapVersion % Test,
+    "com.typesafe.play"            %% "play-test"                 % "2.8.19"         % Test,
+    "org.scalactic"                %% "scalactic"                 % "3.2.15"         % Test,
+    "org.scalatest"                %% "scalatest"                 % "3.2.15"         % "test, it",
+    "org.scalacheck"               %% "scalacheck"                % "1.17.0"         % Test,
+    "org.scalatestplus.play"       %% "scalatestplus-play"        % "5.1.0"          % "test, it",
+    "org.scalatestplus"            %% "scalacheck-1-15"           % "3.2.11.0"       % "test, it",
+    "org.scalatestplus"            %% "mockito-3-4"               % "3.2.10.0"       % "test, it",
+    "uk.gov.hmrc"                  %% "service-integration-test"  % "1.3.0-play-28"  % "test, it",
+    "org.mockito"                  %% "mockito-scala"             % "1.17.14"        % "test, it",
+    "com.github.tomakehurst"       %  "wiremock"                  % "2.33.2"         % "test, it",
+    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.4"         % "test, it",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.15.0"         % "test, it",
   )
 }
