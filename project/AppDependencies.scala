6ea0e88a5d2a6f72c027dcde15521e3a6017145e
Andrew Walker <andrew.walker@digital.hmrc.gov.uk>
2023-08-14 11:58:58 +0100

    DC-5351: Version updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "7.21.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
     "com.typesafe.play" %% "play-json-joda"            % "2.9.4",
-    "org.typelevel"     %% "cats-core"                 % "2.9.0",
+    "org.typelevel"     %% "cats-core"                 % "2.10.0",
   )
 
   // Several dependencies require a lower major version of Scala 2.13 xml library
@@ -20,16 +20,16 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"    % bootstrapVersion % Test,
-    "com.typesafe.play"            %% "play-test"                 % "2.8.19"         % Test,
-    "org.scalactic"                %% "scalactic"                 % "3.2.15"         % Test,
-    "org.scalatest"                %% "scalatest"                 % "3.2.15"         % "test, it",
+    "com.typesafe.play"            %% "play-test"                 % "2.8.20"         % Test,
+    "org.scalactic"                %% "scalactic"                 % "3.2.16"         % Test,
+    "org.scalatest"                %% "scalatest"                 % "3.2.16"         % "test, it",
     "org.scalacheck"               %% "scalacheck"                % "1.17.0"         % Test,
     "org.scalatestplus.play"       %% "scalatestplus-play"        % "5.1.0"          % "test, it",
     "org.scalatestplus"            %% "scalacheck-1-15"           % "3.2.11.0"       % "test, it",
     "org.scalatestplus"            %% "mockito-3-4"               % "3.2.10.0"       % "test, it",
     "org.mockito"                  %% "mockito-scala"             % "1.17.14"        % "test, it",
     "com.github.tomakehurst"       %  "wiremock"                  % "2.33.2"         % "test, it",
-    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.4"         % "test, it",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.15.0"         % "test, it",
+    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.8"         % "test, it",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.15.2"         % "test, it",
   )
 }
