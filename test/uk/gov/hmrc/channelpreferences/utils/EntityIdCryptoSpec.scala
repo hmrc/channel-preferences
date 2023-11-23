d28a46535764e32e8575dc3251c1bdc5a90b5947
Murali raju <17339891+muralishub@users.noreply.github.com>
2023-11-23 09:00:49 +0000

    DC-5367: entity key is now a secret (#104)
    * DC-5367: entity key is now a secret
    
    * DC-5367: entity key is now a secret

--- test/uk/gov/hmrc/channelpreferences/utils/EntityIdCryptoSpec.scala
+++ test/uk/gov/hmrc/channelpreferences/utils/EntityIdCryptoSpec.scala
@@ -22,16 +22,14 @@ class EntityIdCryptoSpec extends PlaySpec with EntityIdCrypto {
 
   "encryptString" must {
     "Successfully parse a string " in {
-      encryptString("f0ea9b13-82bf-4d1a-9619-bb1e2481f8d8") mustBe Left(
-        "YL4AfL0xsXviYiQtibOWOZVqZlKLwsna7Xq4XsFOLjkdcBJd5KldTXzrL81IsrEZ")
+      encryptString("someKey") mustBe Left("1YSXrdZ+2rKy7fDuGQ9DCA==")
     }
 
   }
 
   "decryptString" must {
     "Successfully parse a string " in {
-      decryptString("YL4AfL0xsXviYiQtibOWOZVqZlKLwsna7Xq4XsFOLjkdcBJd5KldTXzrL81IsrEZ") mustBe Left(
-        "f0ea9b13-82bf-4d1a-9619-bb1e2481f8d8")
+      decryptString("1YSXrdZ+2rKy7fDuGQ9DCA==") mustBe Left("someKey")
     }
 
   }
