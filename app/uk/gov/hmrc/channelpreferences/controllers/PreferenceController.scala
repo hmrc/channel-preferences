245b1758950f149e755fc0e759ffa0000dc708b1
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2026-06-16 10:59:36 +0100

    DC-8998 add an endpoint to update etmp with itsa status

--- app/uk/gov/hmrc/channelpreferences/controllers/PreferenceController.scala
+++ app/uk/gov/hmrc/channelpreferences/controllers/PreferenceController.scala
@@ -132,6 +132,12 @@ class PreferenceController @Inject() (
       }
   }
 
+  def processItsaStatus(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    val mtdItsaId = (request.body \ "mtditsaid").as[String]
+    val isDigital = (request.body \ "isDigital").as[String].toBoolean
+    updateEtmpWithContactPreference(isDigital, mtdItsaId)
+  }
+
   private def updateEtmpWithContactPreference(
     isDigitalStatus: Boolean,
     mtdItsaId: String
