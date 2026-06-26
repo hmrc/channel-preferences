5879d00b0e781711481d9002932a1549fb508ef7
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2026-06-26 13:07:35 +0100

    DC-8998 Add tests for process-itsa-status

--- app/uk/gov/hmrc/channelpreferences/controllers/PreferenceController.scala
+++ app/uk/gov/hmrc/channelpreferences/controllers/PreferenceController.scala
@@ -136,7 +136,7 @@ class PreferenceController @Inject() (
   def processItsaStatus(): Action[JsValue] = Action.async(parse.json) { implicit request =>
     val mtdItsaId = (request.body \ "mtditsaid").as[String]
     val isDigital = (request.body \ "isDigital").as[String].toBoolean
-    updateITSAStatus(StatusUpdate(mtdItsaId, isDigital))
+    updateEtmpWithContactPreference(isDigital, mtdItsaId)
   }
 
   private def updateEtmpWithContactPreference(
@@ -210,21 +210,22 @@ class PreferenceController @Inject() (
     Action.async(parse.json) { implicit request =>
       key.toUpperCase() match {
         case ITSA_REGIME =>
-          withJsonBody[StatusUpdate](update => updateITSAStatus(update))
+          withJsonBody[StatusUpdate] { statusUpdate =>
+            statusUpdate.getItsaETMPUpdate match {
+              case Right(itsaETMPUpdate) =>
+                val correlationId = request.headers
+                  .get(CustomHeaders.RequestId)
+                eisContactPreference.updateContactPreference(ITSA_REGIME, itsaETMPUpdate, correlationId).map {
+                  response =>
+                    Status(response.status)(response.json)
+                }
+              case Left(err) =>
+                Future.successful(BadRequest(err))
+            }
+
+          }
         case _ => Future.successful(BadRequest(s"The key $key is not supported"))
       }
-    }
-
-  private def updateITSAStatus(statusUpdate: StatusUpdate)(implicit request: Request[JsValue]): Future[Result] =
-    statusUpdate.getItsaETMPUpdate match {
-      case Right(itsaETMPUpdate) =>
-        val correlationId = request.headers
-          .get(CustomHeaders.RequestId)
-        eisContactPreference.updateContactPreference(ITSA_REGIME, itsaETMPUpdate, correlationId).map { response =>
-          Status(response.status)(response.json)
-        }
-      case Left(err) =>
-        Future.successful(BadRequest(err))
     }
 
   private def auditConfirm(
